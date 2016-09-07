ece3771dbd952d3103755f1b2f836f456cd9fb81
Jeremy Custenborder <jcustenborder@gmail.com>
2016-09-07 16:28:34 -0500

    Cleanup

--- src/main/java/io/confluent/connect/solr/sink/HttpSolrSinkConnector.java
+++ src/main/java/io/confluent/connect/solr/sink/HttpSolrSinkConnector.java
@@ -1,10 +1,16 @@
 package io.confluent.connect.solr.sink;
 
+import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.Task;
 
 public class HttpSolrSinkConnector extends SolrSinkConnector {
   @Override
   public Class<? extends Task> taskClass() {
     return HttpSolrSinkTask.class;
+  }
+
+  @Override
+  public ConfigDef config() {
+    return null;
   }
 }
